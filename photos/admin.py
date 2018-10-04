055c048f293fc2bb3519bdc7562d6ed3a732e947
IreriVikki <wambsviki@gmail.com>
2018-10-04 11:09:59 +0300

    added a user profile model that will store user details to the database

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import UserProfile, Location, tags, Photos
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'gender', 'phone']
+
+
+class PhotosAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(tags)
+admin.site.register(Location)
+admin.site.register(Photos)
--- photos/models.py
+++ photos/models.py
@@ -1,7 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='images/', blank=True)
+    bio = models.TextField(blank=True)
+    gender = models.CharField(max_length=1, choices=(
+        ('M', 'Male'), ('F', 'Female'), ('O', 'Other')), blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    phone = models.PositiveIntegerField(blank=True)
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
 class Location(models.Model):
-    locatoin = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.location
+
+
+class tags(models.Model):
+    tag = models.CharField(max_length=100)
+
+    def save_tag(self):
+        self.save()
+
+
+class Photos(models.Model):
+    photo = models.ImageField(upload_to='images/')
+    caption = models.TextField(blank=True)
+    location = models.ManyToManyField(Location)
