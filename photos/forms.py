90180de6f0339008f92a14f35a3e91716177986c
IreriVikki <wambsviki@gmail.com>
2018-10-08 08:09:19 +0300

    added a commentform class

--- photos/forms.py
+++ photos/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from .models import Photo, UserProfile
+from .models import Photo, UserProfile, Comment
 
 
 class NewPhotoForm(forms.ModelForm):
@@ -15,4 +15,11 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         exclude = ['user']
+        list_display = []
+
+
+class CommentForm (forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['author', 'photo']
         list_display = []
